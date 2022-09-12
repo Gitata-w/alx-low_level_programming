042dc76fecc026db73a8ac502cda4283c8f765da
Gitata-w <wgitata@gmail.com>
2022-09-12 17:11:29 +0300

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *main - Print the string in the put function
- *Description: using the main function
- *Return : Always 0 (Succcess)
- */
-
+*main - Print the string in the put function
+*
+*Description: using the main function
+*Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multiligual puzzle");
