9350ed32682f2f2d478f425a372e6b32734badac
Gitata-w <wgitata@gmail.com>
2022-09-12 15:45:35 +0300

    Puts

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return : Always 0 (Succcess)
+ */
+
+ int main(void)
+ {
+ 	char *i = "Programming is like building a multiligual puzzle";
+	puts(i);
+	return(0);
+ }
+ 
