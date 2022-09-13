d3a6ec1306679137642fe217450ab5f0b8fafc8b
Gitata-w <wgitata@gmail.com>
2022-09-13 04:28:05 +0300

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: Single digit numbers
+*Return: 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
