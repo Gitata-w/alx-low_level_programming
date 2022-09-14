b792e42f61082556592dfcaf95054eed53ce84bf
Gitata-w <wgitata@gmail.com>
2022-09-15 02:00:49 +0300

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - Entry point of the program
+*
+*Description: Prints the putchar character
+*Return: 0
+*/
+
+int main(void)
+{
+	printf("_putchar \n");
+	return (0);
+}
